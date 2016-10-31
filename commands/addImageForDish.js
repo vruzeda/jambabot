82c627cb087efe907289d0641d92e147f280ac75
Vinicius Uzeda <vruzeda@gmail.com>
2016-10-31 13:22:35 -0200

    Removing link's tags when adding dish images.

--- commands/addImageForDish.js
+++ commands/addImageForDish.js
@@ -3,7 +3,7 @@
   var mongodb = require('../integrations/mongodb.js');
 
   function addImageForDish(callback, dishName, dishImageUrl) {
-    mongodb.addImageForDish(dishName, dishImageUrl, function(error) {
+    mongodb.addImageForDish(dishName, dishImageUrl.replace(/^<(.*)>$/, '$1'), function(error) {
       if (error) {
         callback('Vixxxxxxi c lascou kkkkk');
         return;
