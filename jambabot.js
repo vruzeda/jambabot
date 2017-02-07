3b091f20ec7360a8a1e1f4c42955bc4727351d34
Vinicius Uzeda <vruzeda@gmail.com>
2017-02-07 15:49:39 -0200

    Using botkit to implement userbot features.

--- jambabot.js
+++ jambabot.js
@@ -3,8 +3,11 @@
   var bodyParser = require('body-parser');
   var express = require('express');
 
+  var Botkit = require('botkit');
+
   var variables = require('./variables');
   var parseCommand = require('./commands/parseCommand');
+  var commands = require('./commands/commands');
 
   var app = express();
 
@@ -35,6 +38,29 @@
   app.listen(6001, function () {
     console.log('jambabot app listening on port 6001!');
     console.log('variables: ' + JSON.stringify(variables));
+  });
+
+  var controller = Botkit.slackbot({
+    debug: false
+  });
+
+  controller.spawn({
+    token: variables.JAMBABOT_USER_TOKEN,
+  }).startRTM();
+
+  controller.hears('.*', ['direct_message', 'direct_mention', 'mention'], function(bot, botMessage) {
+    bot.api.users.info({user: botMessage.user}, function(error, usersInfoResponse) {
+      var message = {
+        userName: usersInfoResponse.user.name,
+        userText: botMessage.text.replace(/\s+/g, ' ').trim()
+      };
+
+      parseCommand(message, function(response) {
+        if (response) {
+          bot.reply(botMessage, response);
+        }
+      });
+    });
   });
 
 })();
